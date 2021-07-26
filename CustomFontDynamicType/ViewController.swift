2acc8a044520aab52f89e5c852cdd05c9e3785b9
Allicia Viona <alliciawn@gmail.com>
2021-07-26 07:43:48 +0700

    starter project fix

--- CustomFontDynamicType/ViewController.swift
+++ CustomFontDynamicType/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
-        txtLabel.font = appFont.fontMetricsLarge.scaledFont(for: appFont.fontLarge!)
+        
     }
 
 
